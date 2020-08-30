7bff80666eb1f82bcf1393a50ee1f71aadd74ef4
Eredost <michael.coutin62@gmail.com>
2020-08-30 17:35:58 +0200

    fix: modify http code to 201 Created when creating new sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -22,7 +22,7 @@ exports.createSauce = (req, res, next) => {
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
-        .then(() => res.status(200).json({ message: 'Sauce ajoutée !' }))
+        .then(() => res.status(201).json({ message: 'Sauce ajoutée !' }))
         .catch(error => {
             console.log(error);
             return res.status(400).json({ error });
